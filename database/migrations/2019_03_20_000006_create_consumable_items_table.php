67543d6d044363837a072d8d346d9878507f28e9
sckang511 <33492170+sckang511@users.noreply.github.com>
2019-03-29 15:40:46 -0500

    Update 2019_03_20_000006_create_consumable_items_table.php

--- database/migrations/2019_03_20_000006_create_consumable_items_table.php
+++ database/migrations/2019_03_20_000006_create_consumable_items_table.php
@@ -15,9 +15,10 @@ class CreateConsumableItemsTable extends Migration
     {
         Schema::create('consumable_items', function (Blueprint $table) {
             $table->increments('consumable_item_id');
-            $table->unsignedInteger('consumable_item_mapping_id');
-            $table->foreign('consumable_item_mapping_id')->references('consumable_item_mapping_id')->on('consumable_item_mappings');
+            $table->unsignedInteger('consumable_collection_id');
+            $table->foreign('consumable_collection_id')->references('consumable_collection_id')->on('consumable_collections');
             $table->string('item_id');
+            $table->string('item_name');
             $table->double('serving', 8, 2);
             $table->timestamps();
         });
