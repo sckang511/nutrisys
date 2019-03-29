918104e131c53b777bc79724c9a187a76b270dd7
sckang511 <33492170+sckang511@users.noreply.github.com>
2019-03-29 15:39:03 -0500

    Update and rename 2019_03_20_000005_create_consumable_collections_table.php to 2019_03_20_000003_create_consumable_collections_table.php

--- database/migrations/2019_03_20_000003_create_consumable_collections_table.php
+++ database/migrations/2019_03_20_000003_create_consumable_collections_table.php
@@ -15,10 +15,8 @@ class CreateConsumableCollectionsTable extends Migration
     {
         Schema::create('consumable_collections', function (Blueprint $table) {
             $table->increments('consumable_collection_id');
-            $table->integer('food_collection_mapping_id')->unsigned();
-            $table->foreign('food_collection_mapping_id')->references('food_collection_mapping_id')->on('food_collection_mappings');
-            $table->integer('consumable_item_mapping_id')->unsigned();
-            $table->foreign('consumable_item_mapping_id')->references('consumable_item_mapping_id')->on('consumable_item_mappings');
+            $table->integer('consumable_log_id')->unsigned();
+            $table->foreign('consumable_log_id')->references('consumable_log_id')->on('consumable_logs');
             $table->string('consumable_type');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
