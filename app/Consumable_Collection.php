be9cd396d1f5471c7ffd8a19c0fee37dd9a80fc8
seong-android <seong.c.kang@gmail.com>
2019-03-29 21:13:07 -0500

    updated collections

--- app/Consumable_Collection.php
+++ app/Consumable_Collection.php
@@ -9,6 +9,6 @@ class Consumable_Collection extends Model
     protected $primaryKey = 'consumable_collection_id';
 
     protected $fillable = [
-        'consumable_collection_id', 'user_id', 'consumable_type', 'date',
+        'consumable_collection_id', 'user_id', 'consumable_type',
     ];
 }
