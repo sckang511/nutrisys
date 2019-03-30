db312aa2a8a6676f486921babcab90c6fa1847b3
Sennat <dinksira@gmail.com>
2019-03-29 22:29:09 -0500

    Progress on goal

--- app/Goal.php
+++ app/Goal.php
@@ -6,17 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Goal extends Model
 {
-<<<<<<< HEAD
-    //
-    protected $fillable = [
-        'user_id', 'goal_type', 'nutrition_type', 'value',
-    ];
-}
-=======
     protected $primaryKey = 'goal_id';
 
     protected $fillable = [
-        'goal_id', 'user_id', 'goal_type', 'value', 'nutrition_type',
+        'goal_id', 'user_id', 'goal_type', 'nutrition_type', 'value',
     ];
 }
->>>>>>> 67a640988aab538e62e7324520e7eebe64ec49b0
