9bdd0c929cf39399361e79ffee70cb6d0e1e7d37
Sennat <dinksira@gmail.com>
2019-03-29 22:10:55 -0500

    progress

--- app/Goal.php
+++ app/Goal.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Goal extends Model
 {
     //
+    protected $fillable = [
+        'user_id', 'goal_type', 'nutrition_type', 'value',
+    ];
 }
--- resources/views/goal/progress.blade.php
+++ resources/views/goal/progress.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 @section('content')
 <div class="container">
     <div class="row">
@@ -13,7 +13,7 @@
                     <tr>
                         <th scope="col" class="text-info text-uppercase">Nutrition Facts</th>
                         <th scope="col" class="text-info text-uppercase">Current Progress</th>
-                        <th scope="col" class="text-info text-uppercase">Daily Serving</th>
+                        <th scope="col" class="text-info text-uppercase">Daily Total</th>
                         <th scope="col" class="text-info text-uppercase">Daily Goal</th>
                     </tr>
                     </thead>
@@ -31,14 +31,40 @@
                         <div class="col-md-4"><h1 class="text-danger">No data found</h1></div>
                         @endif
                     </tbody>
-                </table><br><br>
-                <div class="container">
-    
-                <div class="row">
-                </div>
-                </div><br><br><br>
-            </div>   
-        </div>
+                </table>
+            </div><br><br>
+        </div>   
     </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h3 class="text-left text-success">Weekly Goal</h3>
+            <hr style="width: 40%;">
+            <div id="curve_chart" style="width: 700px; height: 400px"></div>
+        </div>
+    </div><br><br><br>
 </div>
+<script type="text/javascript">
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004',  1000,      400],
+        ['2005',  1170,      460],
+        ['2006',  660,       1120],
+        ['2007',  1030,      540]
+      ]);
+
+      var options = {
+        title: 'Weekly Goal',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+      chart.draw(data, options);
+    }
+  </script>
 @endsection
