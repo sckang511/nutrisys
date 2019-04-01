f403da508af5e8ca960e232a84550e66cba6a6ad
sckang511 <sckang511@hotmail.com>
2019-04-01 17:44:38 -0500

    good progress on log

--- app/Http/Controllers/Food/LogController.php
+++ app/Http/Controllers/Food/LogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Food;
 
 use Illuminate\Support\Facades\Auth;
+use App\Consumable_Item;
 use App\Nutrition;
 use App\Consumable_Collection;
 use App\Http\Controllers\Controller;
@@ -23,24 +24,98 @@ class LogController extends Controller
 
         // get todays date
         $today = date('Y-m-d');
+        $tomorrow = date('Y-m-d', strtotime('tomorrow'));
 
         // get all the collections where the date is today
-        $collections = Consumable_Collection::where('date', $today)->get();
+        $builder = Consumable_Collection::whereDate('date', '>=', $today)->where('date', '<=', $tomorrow)->get()->toarray();
+
+        $breakfasts = array();
+        $lunches = array();
+        $dinners = array();
+        $others = array();
+        $snacks = array();
 
         //test
-        echo "<pre>                                   $collections is the collections </pre>";
+        //printf("<pre>                                                ");
+        //print_r($builder);
+        //printf("</pre>");
 
         // from above collections sort by collection type: breakfast ...etc
+        foreach($builder as $collection) {
+            if ($collection['consumable_type'] == 'Breakfast') array_push($breakfasts, $collection);
+            if ($collection['consumable_type'] == 'Lunch') array_push($lunches, $collection);
+            if ($collection['consumable_type'] == 'Dinner') array_push($dinners, $collection);
+            if ($collection['consumable_type'] == 'Other') array_push($others, $collection);
+            if ($collection['consumable_type'] == 'Snack') array_push($snacks, $collection);
+        }
+
 
         // for every collection I have, I need to search for items that match the coll id
+        $breakfast_items = array();
+        $lunch_items = array();
+        $dinner_items = array();
+        $other_items = array();
+        $snack_items = array();
+    
+        foreach($breakfasts as $breakfast) {
+            $id = $breakfast['consumable_collection_id'];
+            $builder = Consumable_Item::where('consumable_collection_id', '=', $id)->get()->toarray();
+            foreach($builder as $item) {
+                array_push($breakfast_items, $item);
+            }
+        } 
+        foreach($lunches as $lunch) {
+            $id = $lunch['consumable_collection_id'];
+            $builder = Consumable_Item::where('consumable_collection_id', '=', $id)->get()->toarray();
+            foreach($builder as $item) {
+                array_push($lunch_items, $item);
+            }
+        } 
+        foreach($dinners as $dinner) {
+            $id = $dinner['consumable_collection_id'];
+            $builder = Consumable_Item::where('consumable_collection_id', '=', $id)->get()->toarray();
+            foreach($builder as $item) {
+                array_push($dinner_items, $item);
+            }
+        } 
+        foreach($others as $other) {
+            $id = $other['consumable_collection_id'];
+            $builder = Consumable_Item::where('consumable_collection_id', '=', $id)->get()->toarray();
+            foreach($builder as $item) {
+                array_push($other_items, $item);
+            }
+        } 
+        foreach($snacks as $snack) {
+            $id = $snack['consumable_collection_id'];
+            $builder = Consumable_Item::where('consumable_collection_id', '=', $id)->get()->toarray();
+            foreach($builder as $item) {
+                array_push($snack_items, $item);
+            }
+        } 
 
-        // for every item, I need to get nutrition info to display
+        $breakfast_objects = array();
+        $lunch_objects =array();
+        $dinner_objects = array();
+        $other_objects = array();
+        $snack_objects = array();
 
-        // 
+        for($i = 0; $i < sizeof($breakfast_items) - 1; $i++) {
+            $id = $breakfast_items[$i]['nutrition_id'];
+            $builder = Nutrition::where('nutrition_id', '=', $id)->get()->toarray();
+            $info = $builder[0];
+            $breakfast_objects += array(
+                $breakfast_items[$i]['consumable_item_id'] => $info,
+            );
+        }
 
+        // TO:DO:: I am losing one breakfast item id 25 for some reason. need to figure it out
 
-        $nutrition = Nutrition::all();
-        return view('food.log')->with('nutrition', $nutrition);
+        $size = sizeof($breakfast_items);
+        printf("<pre>                                                $size Breakfast object");
+        print_r($breakfast_objects);
+        printf("</pre>");
+        return view('food.log');
+        //return view('food.log')->with('nutrition', $nutrition);
     }
 
     /**
