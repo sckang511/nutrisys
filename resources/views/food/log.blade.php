703f1de4ece4754b9f97e7ea6b68cfa6ed955097
seong-android <seong.c.kang@gmail.com>
2019-04-01 23:26:29 -0500

    adjusted log style

--- resources/views/food/log.blade.php
+++ resources/views/food/log.blade.php
@@ -169,11 +169,11 @@
                         });
 
                         $("document").ready(function() {
-                            $("#breakfast").click();
-                            $("#lunch").click();
-                            $("#dinner").click();
-                            $("#other").click();
-                            $("#snack").click();
+                            //$("#breakfast").click();
+                            //$("#lunch").click();
+                            //$("#dinner").click();
+                            //$("#other").click();
+                            //$("#snack").click();
                         });
 
                         function viewDetails(info) {
