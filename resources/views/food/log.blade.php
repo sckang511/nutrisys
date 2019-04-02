3f91c6630456ef7775a25402b052418d028291ff
sckang511 <sckang511@hotmail.com>
2019-04-01 20:41:10 -0500

    Log basic implementation done

--- resources/views/food/log.blade.php
+++ resources/views/food/log.blade.php
@@ -25,10 +25,22 @@
                                     <span>Breakfast</span>
                                     <div class="food-log-table-dropdown-submenu" id="breakfast-sub">
                                     <ul>
-                                        <li>
-                                        </li>
-                                        <li>
-                                        </li>                
+                                        @if (!empty($results['Breakfast']))
+                                            @foreach ($results['Breakfast'] as $breakfast)
+                                                <pre>
+                                                    <img src={{$breakfast['item_image']}} width="150" height="150" style="float:left;">
+                                                    <span style="float:left;">Item name: {{$breakfast['item_name']}}</span>
+                                                    <span style="float:left;">Serving qty: {{$breakfast['serving_qty']}}</span>
+                                                    <span style="float:left;">Serving unit: {{$breakfast['serving_unit']}}</span>
+                                                    <span style="float:left;">Calories: {{$breakfast['calorie']}}</span>
+                                                    <span style="float:left;">Carbohydrates: {{$breakfast['carbohydrate']}}</span>
+                                                    <span style="float:left;">Protein: {{$breakfast['protein']}}</span>
+                                                    <button type='button' class='btn btn-info' style='float:left;'>View Details</button>
+                                                </pre>
+                                                
+                                            @endforeach
+                                        @endif
+
                                     </ul>
                                     </div>
                             </li>     
@@ -37,10 +49,21 @@
                                     <span>Lunch</span>
                                     <div class="food-log-table-dropdown-submenu" id="lunch-sub">
                                     <ul>
-                                        <li>
-                                        </li>
-                                        <li>
-                                        </li>                
+                                        @if (!empty($results['Lunch']))
+                                            @foreach ($results['Lunch'] as $lunch)
+                                                <pre>
+                                                    <img src={{$lunch['item_image']}} width="150" height="150" style="float:left;">
+                                                    <span style="float:left;">Item name: {{$lunch['item_name']}}</span>
+                                                    <span style="float:left;">Serving qty: {{$lunch['serving_qty']}}</span>
+                                                    <span style="float:left;">Serving unit: {{$lunch['serving_unit']}}</span>
+                                                    <span style="float:left;">Calories: {{$lunch['calorie']}}</span>
+                                                    <span style="float:left;">Carbohydrates: {{$lunch['carbohydrate']}}</span>
+                                                    <span style="float:left;">Protein: {{$lunch['protein']}}</span>
+                                                    <button type='button' class='btn btn-info' style='float:left;'>View Details</button>
+                                                </pre>
+                                                
+                                            @endforeach
+                                        @endif
                                     </ul>
                                     </div>
                             </li>           
@@ -49,10 +72,22 @@
                                     <span>Dinner</span>
                                     <div class="food-log-table-dropdown-submenu" id="dinner-sub">
                                     <ul>
-                                        <li>
-                                        </li>
-                                        <li>
-                                        </li>                
+                                        @if (!empty($results['Dinner']))
+                                            @foreach ($results['Dinner'] as $dinner)
+                                                <pre>
+                                                    <img src={{$dinner['item_image']}} width="150" height="150" style="float:left;">
+                                                    <span style="float:left;">Item name: {{$dinner['item_name']}}</span>
+                                                    <span style="float:left;">Serving qty: {{$dinner['serving_qty']}}</span>
+                                                    <span style="float:left;">Serving unit: {{$dinner['serving_unit']}}</span>
+                                                    <span style="float:left;">Calories: {{$dinner['calorie']}}</span>
+                                                    <span style="float:left;">Carbohydrates: {{$dinner['carbohydrate']}}</span>
+                                                    <span style="float:left;">Protein: {{$dinner['protein']}}</span>
+                                                    <button type='button' class='btn btn-info' style='float:left;'>View Details</button>
+                                                </pre>
+                                                
+                                            @endforeach
+                                        @endif
+
                                     </ul>
                                     </div>
                             </li>   
@@ -61,10 +96,21 @@
                                     <span>Other</span>
                                     <div class="food-log-table-dropdown-submenu" id="other-sub">
                                     <ul>
-                                        <li>
-                                        </li>
-                                        <li>
-                                        </li>                
+                                        @if (!empty($results['Other']))
+                                            @foreach ($results['Other'] as $other)
+                                                <pre>
+                                                    <img src={{$other['item_image']}} width="150" height="150" style="float:left;">
+                                                    <span style="float:left;">Item name: {{$other['item_name']}}</span>
+                                                    <span style="float:left;">Serving qty: {{$other['serving_qty']}}</span>
+                                                    <span style="float:left;">Serving unit: {{$other['serving_unit']}}</span>
+                                                    <span style="float:left;">Calories: {{$other['calorie']}}</span>
+                                                    <span style="float:left;">Carbohydrates: {{$other['carbohydrate']}}</span>
+                                                    <span style="float:left;">Protein: {{$other['protein']}}</span>
+                                                    <button type='button' class='btn btn-info' style='float:left;'>View Details</button>
+                                                </pre>
+                                                
+                                            @endforeach
+                                        @endif
                                     </ul>
                                     </div>
                             </li>  
@@ -72,7 +118,23 @@
                             <li class="food-log-table-dropdown" id="snack">
                                     <span>Snack</span>
                                     <div class="food-log-table-dropdown-submenu" id="snack-sub">
-                                    
+                                    <ul>
+                                        @if (!empty($results['Snack']))
+                                            @foreach ($results['Snack'] as $snack)
+                                                <pre>
+                                                    <img src={{$snack['item_image']}} width="150" height="150" style="float:left;">
+                                                    <span style="float:left;">Item name: {{$snack['item_name']}}</span>
+                                                    <span style="float:left;">Serving qty: {{$snack['serving_qty']}}</span>
+                                                    <span style="float:left;">Serving unit: {{$snack['serving_unit']}}</span>
+                                                    <span style="float:left;">Calories: {{$snack['calorie']}}</span>
+                                                    <span style="float:left;">Carbohydrates: {{$snack['carbohydrate']}}</span>
+                                                    <span style="float:left;">Protein: {{$snack['protein']}}</span>
+                                                    <button type='button' class='btn btn-info' style='float:left;'>View Details</button>
+                                                </pre>
+                                                
+                                            @endforeach
+                                        @endif
+                                    </ul>
                                     </div>
                             </li>  
                         </ul>
